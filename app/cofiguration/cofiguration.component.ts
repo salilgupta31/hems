2392ce8270190f4991216ac27963daba6ba472c6
salilgupta31 <31129094+salilgupta31@users.noreply.github.com>
2018-03-06 15:08:07 +0800

    Add files via upload

---
+++ app/cofiguration/cofiguration.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-cofiguration',
+  templateUrl: './cofiguration.component.html',
+  styleUrls: ['./cofiguration.component.css'],
+  // encapsulation:ViewEncapsulation.Native,
+})
+export class CofigurationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
